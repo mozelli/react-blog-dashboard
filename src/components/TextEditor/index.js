f7b725b61e5d8791a19602522d9a8bc98794abd0
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-04-08 10:47:01 -0300

    post editor styles

---
+++ src/components/TextEditor/index.js
@@ -0,0 +1,67 @@
+import { useCallback, useState } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+//import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+
+import styles from './editor.module.scss';
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", 'underline'],
+    [{ color: [] }, { background:[] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"]
+]
+
+const TextEditor = () => {
+
+    const [postTitle, setPostTitle] = useState("");
+
+    //const [quill, setQuill] = useState();
+
+    const wrapperRef = useCallback((wrapper) => {
+        if(wrapper == null) return
+        
+        const editor = document.createElement('div');
+        wrapper.append(editor);
+        new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+        //const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+
+        //setQuill(q);
+        
+    }, []);
+
+    // function teste() {
+    //     let delta = quill.clipboard.quill.editor.delta.ops;
+    //     let cfg = {};
+    //     let converter = new QuillDeltaToHtmlConverter(delta, cfg);
+    //     let html = converter.convert();
+
+    //     console.log(html);
+    // }
+
+    return (
+        <div className={ styles.editor }>
+            <div className="formGroup">
+                <input 
+                    type="text" 
+                    className={ styles.inputTitle } 
+                    placeholder="Título"
+                    value={ postTitle }
+                    onChange={ (event) => setPostTitle(event.target.value) } />
+            </div>
+            
+            <div id="container" ref={wrapperRef}></div>
+            <div className="buttonGroup">
+                <button type='button' className={ styles.saveButton }><i class="ri-save-3-line ri-xl"></i> Salvar rascunho</button>
+                <button type='button' className={ styles.publishButton }><i class="ri-check-double-line ri-xl"></i> Publicar</button>
+            </div>
+        </div>
+    );
+}
+
+export default TextEditor;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
 import Header from '../../components/Header';
-import Posts from '../../components/Posts';
+// import Posts from '../../components/Posts';
 import styles from './home.module.scss';
+
+import TextEditor from '../../components/TextEditor';
 
 const Home = () => {
     return (
         <div>
+            
             <div className={ styles.header }>
                 <Header />
             </div>
+            <TextEditor />
             <div className={ styles.main }>
-                <Posts />
+                {/* <Posts /> */}
             </div>
         </div>
     );
