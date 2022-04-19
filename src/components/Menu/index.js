96ed4db534ef28ff751248f6ee025f721028fba9
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-04-19 15:05:24 -0300

    create dashboard menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,8 +2,17 @@ import styles from './menu.module.scss';
 
 const Menu = () => {
     return (
-        <nav>
-            Menu
+        <nav className={ styles.nav }>
+            <h2>Menu</h2>
+            <ul>
+                <li><i class="ri-file-line"></i> Posts</li>
+                <li><i class="ri-price-tag-3-line"></i> Tags</li>
+                <li><i class="ri-bookmark-line"></i> Categorias</li>
+                <li><i class="ri-window-line"></i> Páginas</li>
+                <li><i class="ri-t-shirt-2-line"></i> Temas</li>
+                <li><i class="ri-user-line"></i> Usuários</li>
+
+            </ul>
         </nav>
     );
 }
